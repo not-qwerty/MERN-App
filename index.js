7f78ab61084ab38454934708b6d8cb22b23b8535
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-07-04 14:15:30 +0300

    added semicolon

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const PORT = process.env.PORT || 5000;
 
