4d96fb89a0c03455faec408b8b8633966701b182
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-07-05 19:22:01 +0300

    restructured application server and added black navbar

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const app = express();
 require('./config/prod')(app);
 
 
-//MODELS
-const Post = require('./models/Post');
-
-
 // DATABASE CONNECT
 mongoose.connect('mongodb+srv://max:bi565KdUsZkxvLNt@cluster0-zsw8p.mongodb.net/CHAT?retryWrites=true&w=majority', {
         useNewUrlParser: true, useUnifiedTopology: true
@@ -26,69 +22,9 @@ app.use('/', express.static('public'))
 
 
 // ROUTES
-app.get('/posts', async (req, res, next) => {
-    try {
-        const posts = await Post.find();
-        res.send(posts)
-    } catch (err) {
-        console.error(err.message || err)
-    }
 
-})
+app.use('/posts', require('./routes/posts'))
 
-app.get('/posts/:id', async (req, res, next) => {
-    try {
-        const { id } = req.params;
-        const post = await Post.findById(id);
-        res.send(post)
-        next();
-    } catch (err) {
-        console.error(err.message || err)
-    }
-
-})
-
-app.post('/posts', (req, res, next) => {
-    const { name, postHeader, postBody } = req.body;
-    const post = new Post({
-        name: name,
-        postHeader: postHeader,
-        postBody: postBody
-    })
-
-    post.save()
-    res.send(post)
-
-})
-
-
-// TEST THIS
-app.put('/posts/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const { postBody, postHeader } = req.body;
-        const post = await Post.findByIdAndUpdate(id, {
-            postHeader: postHeader,
-            postBody: postBody
-        })
-
-        res.send('Updated successfully')
-    } catch (err) {
-        console.error(err.message)
-    }
-})
-
-app.delete('/posts/:id', async (req, res) => {
-    try {
-        const { id } = req.params;
-        const deleted = await Post.findByIdAndDelete(id)
-
-        deleted ? res.status(200).send('Deleted successfully') : res.send('Post not found');
-
-    } catch (err) {
-        console.error(err.message)
-    }
-})
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log('server is up and running on port', PORT))
