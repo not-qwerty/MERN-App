fe6d3cced5e4d07529a1365267c782d004c433d3
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-07-19 18:35:00 +0300

    login form

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useRef } from "react";
 
 export default function Login() {
-    return (
-        <div>
-            <h1>Login</h1>
+
+  const emailRef = useRef();
+
+  useEffect(() => {
+    emailRef.current.focus();
+  }, [])
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const email = emailRef.current.value;
+    console.log(email);
+
+
+};
+
+  return (
+    <div className="container">
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="email">Email address</label>
+          <input
+            ref={emailRef}
+            type="email"
+            className="form-control"
+            id="email"
+            placeholder="Enter email"
+          />
         </div>
-    )
+
+        <div className="form-group">
+          <label htmlFor="exampleInputPassword1">Password</label>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            placeholder="Password"
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
