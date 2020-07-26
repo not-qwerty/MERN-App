2bd844f35b915e4d6cd26253e8989a76e1261ff5
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-07-26 18:27:27 +0300

    setting back cors

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const helmet = require("helmet");
-// const cors = require("cors");
+const cors = require("cors");
 const path = require("path");
 const cookieParser = require("cookie-parser");
 
@@ -20,7 +20,7 @@ require("./db/index")();
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser());
-// app.use(cors());
+app.use(cors());
 
 require("./routes/routes")(app);
 
