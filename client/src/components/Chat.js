577d4554a26e7edb6e696cd132f934d71a2fc89d
Bazadji Maxim <maximbazadji@safeguardglobal.com>
2020-07-28 09:25:39 +0300

    small fixed on chat

--- client/src/components/Chat.js
+++ client/src/components/Chat.js
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import io from "socket.io-client";
 import API from "../utils/httpService";
+import UserContext from '../context/UserContext';
 
 const socket = io.connect(API);
 
 export default function App() {
-  const [state, setState] = useState({ message: "", name: "" });
+  const [state, setState] = useState({ message: ""});
   const [chat, setChat] = useState([]);
+
+  const userContext = useContext(UserContext);
 
   useEffect(() => {
     socket.on("message", ({ name, message }) => {
@@ -20,9 +23,10 @@ export default function App() {
 
   const onMessageSubmit = (e) => {
     e.preventDefault();
-    const { name, message } = state;
+    const { message } = state;
+    const { name } = userContext.user;
     socket.emit("message", { name, message });
-    setState({ message: "", name });
+    setState({ message: ""});
   };
 
   const renderChat = () => {
@@ -36,24 +40,16 @@ export default function App() {
   };
 
   return (
-    <div className="card">
+    <div className="container">
       <form onSubmit={onMessageSubmit}>
         <h1>Messanger</h1>
-        <div className="name-field">
-          <input
-            name="name"
-            onChange={(e) => onTextChange(e)}
-            value={state.name}
-            label="Name"
-          />
-        </div>
+
         <div>
           <input
+            autoFocus
             name="message"
             onChange={(e) => onTextChange(e)}
             value={state.message}
-            id="outlined-multiline-static"
-            variant="outlined"
             label="Message"
           />
         </div>
